878320c8a3a4940590e62f330b55218160577d95
Congcong Cai <congcongcai0907@163.com>
2022-02-06 01:16:51 +0800

    feat: re-design UI

--- src/web/app.tsx
+++ src/web/app.tsx
@@ -1,7 +1,8 @@
 import { ipcRenderer } from "electron";
 import React, { Component } from "react";
-import { Button, Typography } from "antd";
+import { Button, Card, Col, Row, Typography, Layout } from "antd";
 const { Title } = Typography;
+const { Content, Footer, Header } = Layout;
 import "./app.css";
 import { GenerateSvgArg } from "../common/generateInterface";
 import { SvgDisplayer } from "./svgDisplayer";
@@ -11,8 +12,8 @@ import { filePaths } from "./global";
 export class App extends Component<{}, { svgPath: string[] }> {
   onclick = () => {
     let arg: GenerateSvgArg = {
-      x: 500,
-      y: 500,
+      x: 300,
+      y: 300,
       path: filePaths.filePaths,
     };
     ipcRenderer.send("generate_svg", arg);
@@ -20,16 +21,36 @@ export class App extends Component<{}, { svgPath: string[] }> {
 
   render() {
     return (
-      <div>
-        <Title level={2}>词云生成器</Title>
-        <Uploader />
-        <div>
-          <Button type="primary" onClick={this.onclick}>
-            Button
-          </Button>
-        </div>
-        <SvgDisplayer />
-      </div>
+      <Layout className="layout">
+        <Header style={{ height: "fit-content" }}>
+          <Title style={{ color: "white" }} level={4}>
+            词云生成器
+          </Title>
+        </Header>
+        <Content style={{ padding: "0 5px" }}>
+          <div className="site-card-wrapper">
+            <Row gutter={24} align="top">
+              <Col span={12}>
+                <Card title="编辑区" bordered={true}>
+                  <Uploader />
+                  <br />
+                  <div style={{ textAlign: "center" }}>
+                    <Button type="primary" onClick={this.onclick}>
+                      生成词云
+                    </Button>
+                  </div>
+                </Card>
+              </Col>
+              <Col span={12}>
+                <Card title="预览区" bordered={true}>
+                  <SvgDisplayer />
+                </Card>
+              </Col>
+            </Row>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>Ant Design ©2018 Created by Ant UED</Footer>
+      </Layout>
     );
   }
 }
--- src/web/svgDisplayer.tsx
+++ src/web/svgDisplayer.tsx
@@ -1,27 +1,50 @@
-import { ipcRenderer } from "electron";
+import { ipcRenderer, IpcRendererEvent } from "electron";
 import React, { Component } from "react";
 import { GenerateSvgReply } from "../common/generateInterface";
 
 const colorSet = ["#86E3CE", "#D0E6A5", "#FFDD94", "#FA897B", "#CCABD8"];
 
-export class SvgDisplayer extends Component<{}, { svgPath: string[] }> {
-  constructor(props: {}) {
+type P = {};
+type S = { l: number; svgPath: string[] };
+
+export class SvgDisplayer extends Component<P, S> {
+  myRef;
+  constructor(props: P) {
     super(props);
     this.state = {
+      l: 300,
       svgPath: [],
     };
-    ipcRenderer.on("generate_svg_reply", (event, data: GenerateSvgReply) => {
-      this.setState({ svgPath: data });
-    });
+    this.myRef = React.createRef<HTMLDivElement>();
   }
 
-  render() {
+  onChangeSize = () => {
+    if (this.myRef.current) {
+      this.setState({
+        l: this.myRef.current.clientWidth,
+      });
+    }
+  };
+  onRecvData = (event: IpcRendererEvent, data: GenerateSvgReply) => {
+    this.setState({ svgPath: data });
+  };
+
+  override componentDidMount = () => {
+    this.onChangeSize();
+    ipcRenderer.on("generate_svg_reply", this.onRecvData);
+    window.addEventListener("resize", this.onChangeSize); //监听窗口大小改变
+  };
+  override componentWillUnmount = () => {
+    ipcRenderer.off("generate_svg_reply", this.onRecvData);
+    window.removeEventListener("resize", this.onChangeSize); //监听窗口大小改变
+  };
+
+  override render() {
     return (
-      <div>
-        <svg width="500" height="500" xmlns="http://www.w3.org/2000/svg" version="1.1">
-          <title>Example triangle01- simple example of a 'path'</title>
-          <rect x="1" y="1" width="499" height="499" fill="none" stroke="pink" strokeWidth={5} />
-          <desc>A path that draws a triangle</desc>
+      <div ref={this.myRef}>
+        <svg width={this.state.l} height={this.state.l} xmlns="http://www.w3.org/2000/svg" version="1.1">
+          {/* <title>词云</title> */}
+          <rect x="1" y="1" width={this.state.l - 1} height={this.state.l - 1} fill="none" stroke="pink" strokeWidth={5} />
           {this.state.svgPath.map((v, i) => (
             <path d={v} key={i} fill={colorSet[Math.floor(Math.random() * colorSet.length)]} />
           ))}
