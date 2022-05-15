c047d478995041329b1f4f0e09e3ba0ee790c893
HerrCai <congcongcai0907@163.com>
2022-05-15 21:05:48 +0800

    fix: 使用中文作为主题色的键

--- src/common/colorThema.ts
+++ src/common/colorThema.ts
@@ -6,8 +6,8 @@ interface Thema {
 }
 
 export const colorThemas: Thema[] = [
-  { id: "blue", colors: ["#5B3265", "#5B9AFF", "#3C77BA"] },
-  { id: "red", colors: ["#FD9A28", "#FF5126", "#D50000"] },
+  { id: "蓝色", colors: ["#5B3265", "#5B9AFF", "#3C77BA"] },
+  { id: "红色", colors: ["#FD9A28", "#FF5126", "#D50000"] },
 ];
 
 export const defaultThema: ColorThema = colorThemas[0].id;
